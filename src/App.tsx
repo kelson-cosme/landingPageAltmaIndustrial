4a4550b8ebc586f6f1753023165e9ec24b969003
Kelson Cosme <kelson.almeida123@gmail.com>
2025-05-30 16:43:13 -0400

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,44 @@
+
+import './index.css'
+import { NavbarDemo } from './components/Navbar'
+
+function App() {
+
+  return (  
+    <div className="relative w-full  bg-amber-300">
+      <NavbarDemo/>
+
+      <h1>1</h1>
+      <h1>teste</h1>
+      <h1>teste</h1>
+      <h1>teste</h1>
+      <h1>teste</h1>
+      <h1>teste</h1>
+      <h1>teste</h1>
+      <h1>teste</h1>
+      <h1>teste</h1>
+      <h1>teste</h1>
+      <h1>teste</h1>
+      <h1>teste</h1>
+      <h1>teste</h1>
+      <h1>teste</h1>
+      <h1>teste</h1>
+      <h1>teste</h1>
+      <h1>teste</h1>
+      <h1>teste</h1>
+      <h1>teste</h1>
+      <h1>teste</h1>
+      <h1>teste</h1>
+      <h1>teste</h1>
+      <h1>teste</h1>
+      <h1>teste</h1>
+      <h1>teste</h1>
+      <h1>teste</h1>
+      <h1>teste</h1>
+      <h1>teste</h1>
+    </div>
+  )
+}
+
+export default App
+
