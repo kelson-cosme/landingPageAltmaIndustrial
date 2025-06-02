53d5cb1501045ae725a6ef35114fd09b4ede777f
Kelson Cosme <kelson.almeida123@gmail.com>
2025-06-02 16:34:23 -0400

    tste

--- vite.config.ts
+++ vite.config.ts
@@ -1,9 +1,12 @@
 import path from "path"
-import tailwindcss from "@tailwindcss/vite"
-import react from "@vitejs/plugin-react"
+import { fileURLToPath } from "url"
 import { defineConfig } from "vite"
+import react from "@vitejs/plugin-react"
+import tailwindcss from "@tailwindcss/vite"
 
-// https://vite.dev/config/
+const __filename = fileURLToPath(import.meta.url)
+const __dirname = path.dirname(__filename)
+
 export default defineConfig({
   plugins: [react(), tailwindcss()],
   resolve: {
